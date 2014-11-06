84da53c2af4505e1248dbd7ff16c0cf98f398c00
ogata.fumitoshi <oggata@gmail.com>
2014-11-06 17:02:15 +0900

    add talk list

--- Meddle/ViewController.swift
+++ Meddle/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //var image = UIImage(named: "headerLogo.png") as UIImage
+        //self.navigationController.navigationBar.setBackgroundImage(image, 
+        //    forBarMetrics: .Default)
+        self.title = "timeLine"
+        //self.view.backgroundColor = UIColor.clearColor()
+        //self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "headerLogo.png"), forBarMetrics: .Default)
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        //self.navigationController?.navigationBar.backgroundColor = UIColor.blueColor()
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 29.0/255.0, green: 202.0/255.0, blue: 255.0/255.0, alpha: 1)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int  {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath:NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "xxxx"
+        //cell.textLabel?.text = self.damsData[indexPath.row]["name"] as String?
+        return cell
+    }
+    
+    func tableView(tableView: UITableView?, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
+    }
 }
 
--- Meddle/WriteCommentController.swift
+++ Meddle/WriteCommentController.swift
@@ -14,10 +14,25 @@ class WriteCommentController: UIViewController {
     @IBAction func backButtonPushed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }    
+    
+    @IBOutlet var writeButton: UIButton!
+    @IBAction func writeButtonPushed(sender: AnyObject) {
+        println("write")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    @IBOutlet var inputComment: UITextView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Write"
+        
+        inputComment.frame = CGRectMake(0, 0, 300, 300)
+        inputComment.allowsEditingTextAttributes = true
+        inputComment.layer.borderWidth = 1
+        inputComment.layer.borderColor = UIColor.blackColor().CGColor
+        inputComment.layer.cornerRadius = 8
     }
     
     override func didReceiveMemoryWarning() {
