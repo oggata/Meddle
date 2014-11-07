2b865fdc2d97a14ee0d3c75fcb831bcb05612290
ogata.fumitoshi <oggata@gmail.com>
2014-11-07 11:32:06 +0900

    add collection view

--- Meddle/Tab2ViewController.swift
+++ Meddle/Tab2ViewController.swift
@@ -8,26 +8,12 @@
 
 import UIKit
 
+
 class Tab2ViewController: UIViewController {
     
+    @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    
-    
-        // Do any additional setup after loading the view, typically from a nib.
-        /*
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        layout.itemSize = CGSize(width: 90, height: 90)
-        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView!.dataSource = self
-        collectionView!.delegate = self
-        collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
-        collectionView!.backgroundColor = UIColor.whiteColor()
-        self.view.addSubview(collectionView!)
-        */
-
     }
 
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -35,22 +21,18 @@ class Tab2ViewController: UIViewController {
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return 5
     }    
-/*
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as CollectionViewCell
-        cell.backgroundColor = UIColor.blackColor()
-        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
-        cell.imageView?.image = UIImage(named: "circle")
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell_identity", forIndexPath: indexPath) as UICollectionViewCell
+        //cell.backgroundColor = UIColor.blackColor()
+        //cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+        //cell.imageView?.image = UIImage(named: "circle")
         return cell
     }
-*/
-    
-    
     
     override func viewDidAppear(animated: Bool) {
-    
         var loginAlert:UIAlertController = UIAlertController(title: "New Identity was created", 
             message: "please change identity from your list", preferredStyle: UIAlertControllerStyle.Alert)
         loginAlert.addAction(UIAlertAction(title: "ok", style: .Default, handler: nil))
